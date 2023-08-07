9a6b8a6aa79ddcaf39c0f488708c6d5674c0fdb4
me_madness <p_rangel@abv.bg>
2023-08-07 14:35:55 +0200

    new try 14:36

--- Seventh lecture_25_07_23/main5.py
+++ Seventh lecture_25_07_23/main5.py
@@ -1,15 +1,16 @@
-# import sys
+import sys
+import csv
 
 new_file_open = 'histogram.txt'
 new_file_open_two = 'original_text.txt'
 max_value = 10000000000000
+histogram = {}
 
 
 try:
     with open(new_file_open_two, 'r') as file:
         # sys.stdout = file
         reading = file.read()
-        histogram = {}
         for x in reading:
             if x in histogram:
                 histogram[x] = histogram[x] + 1
@@ -24,16 +25,17 @@ finally:
     
 try:
     with open(new_file_open, "w") as new_file:
+        csv.reader = csv.reader(new_file)
         for key, val in histogram.items():
-           for i in range[val]:
-                vals = i[val]   
+           for num in val:
+                vals = num.strip().split('\n')  
                 if vals < max_value:
                     max_value = vals
                      
             # for val in sorted(histogram.keys()):
             #     print(f"Count of the symbols='{val}' is {histogram[val]}")                
                     
-                    count_two = f"Count of the value= '{vals}' : {key}" + f"Count of the key= '{key}' : {val}\n"
+                    count_two = f"Count of the max value= '{vals}' : {key}" + f"Count of the key= '{key}' : {val}\n"
                     new_file.write(count_two) 
                 print(str(max_value)) 
 except FileNotFoundError:
