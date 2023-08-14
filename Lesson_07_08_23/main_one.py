9c0620c3e49d3f356ea8fd865b4cde7200432131
me_madness <p_rangel@abv.bg>
2023-08-14 14:50:47 +0200

    Sixth is finish

--- Lesson_07_08_23/main_one.py
+++ Lesson_07_08_23/main_one.py
@@ -55,16 +55,16 @@
 # 6. Sixth task check if an element exists 
 
 empty_list = [1, 3, 5, 8, 6]
-# 6.1 First Way
-print("Please, enter a number")
-try:
-    a = int(input()) 
-    if a in empty_list:
-        print(a, "exists")
-    else:
-        print(a, "Does not exist")     
-except:   
-    print('Your input is not correct')    
+# # 6.1 First Way
+# print("Please, enter a number")
+# try:
+#     a = int(input()) 
+#     if a in empty_list:
+#         print(a, "exists")
+#     else:
+#         print(a, "Does not exist")     
+# except:   
+#     print('Your input is not correct')    
 # 6.2 Second Way    
 print("Please, enter a number")
 while True:
@@ -72,84 +72,90 @@ while True:
         a = int(input()) 
         break   
     except:
-        print('Your input is not correct')        
+        print('Your input is not correct')  
+
+
+if a in empty_list:
+    print(a, "exists")
+else:
+    print(a, "Does not exist")              
     
 # 7. Seven task count the occurrences 
 
-empty_list = [1, 3, 5, 1, 3, 5, 8, 4, 3, 5, 1, 3, 5, 8, 6, 5, 8, 6 ]
-empty_list.count()   
+# empty_list = [1, 3, 5, 1, 3, 5, 8, 4, 3, 5, 1, 3, 5, 8, 6, 5, 8, 6 ]
+# empty_list.count()   
 
-# 8. Eighth task find the index 
+# # 8. Eighth task find the index 
 
-empty_list = [1, 3, 5, 8, 6, 5, 6, 2, 5]
-empty_list.index()
+# empty_list = [1, 3, 5, 8, 6, 5, 6, 2, 5]
+# empty_list.index()
 
-# 9. Ninth task  reverse the order
+# # 9. Ninth task  reverse the order
 
-empty_list = [1, 3, 5, 8, 6, 5, 6, 2, 5]
-empty_list.reverse()
+# empty_list = [1, 3, 5, 8, 6, 5, 6, 2, 5]
+# empty_list.reverse()
 
-# 10. Tenth task sort the list
+# # 10. Tenth task sort the list
 
-empty_list = [1, 3, 5, 8, 6, 9, 0]
-empty_list.sort()
+# empty_list = [1, 3, 5, 8, 6, 9, 0]
+# empty_list.sort()
 
-# 11. Eleventh task remove elements
+# # 11. Eleventh task remove elements
 
-empty_list = [1, 3, 5, 8, 6, 9, 0]
-empty_list.remove()
-empty_list.pop()
+# empty_list = [1, 3, 5, 8, 6, 9, 0]
+# empty_list.remove()
+# empty_list.pop()
 
-# 12. Twelve task  clear all elements
+# # 12. Twelve task  clear all elements
 
-empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-empty_list.clear()
+# empty_list = [1, 3, 5, 8, 6, 9, 0 ]
+# empty_list.clear()
 
-# 13. Thirteen task  create a copy of the list 
+# # 13. Thirteen task  create a copy of the list 
 
-empty_list_two = 0
-empty_list_two.copy(empty_list)
-empty_list = []
+# empty_list_two = 0
+# empty_list_two.copy(empty_list)
+# empty_list = []
 
-# 14. Fourteen task concatenate two lists 
+# # 14. Fourteen task concatenate two lists 
 
-empty_list_one = [1, 3, 5, 8, 6, 9, 0]
-empty_list_two = [ 6, 9, 2, 10]
+# empty_list_one = [1, 3, 5, 8, 6, 9, 0]
+# empty_list_two = [ 6, 9, 2, 10]
 
 
-# 15. Fifteen task use list comprehension
+# # 15. Fifteen task use list comprehension
 
-empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-squares[empty_list]
+# empty_list = [1, 3, 5, 8, 6, 9, 0 ]
+# squares[empty_list]
 
-# 16. Sixteen task combine two lists
+# # 16. Sixteen task combine two lists
 
-empty_list_one = [1, 2, 3]
-empty_list_two = ['a', 'b', 'c']
-zip()
+# empty_list_one = [1, 2, 3]
+# empty_list_two = ['a', 'b', 'c']
+# zip()
 
-# 17. Seventeen task find the maximum and minimum elements  
+# # 17. Seventeen task find the maximum and minimum elements  
 
-empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-max()
-min()
+# empty_list = [1, 3, 5, 8, 6, 9, 0 ]
+# max()
+# min()
 
-# 18. Eighteen task calculate the sum
+# # 18. Eighteen task calculate the sum
 
-empty_list = [1, 3, 5, 8, 6, 9, 0]
-sum()
+# empty_list = [1, 3, 5, 8, 6, 9, 0]
+# sum()
 
-# 19. Nineteen task use the enumerate()
+# # 19. Nineteen task use the enumerate()
 
-empty_list = [1, 3, 5, 8, 6, 9, 0]
-enumerate()
+# empty_list = [1, 3, 5, 8, 6, 9, 0]
+# enumerate()
 
-# 20. Twenty task use list unpacking 
+# # 20. Twenty task use list unpacking 
 
-empty_list = [1, 3, 5, 8]
+# empty_list = [1, 3, 5, 8]
 
-# 21. Twenty-one task remove duplicate elements 
+# # 21. Twenty-one task remove duplicate elements 
 
-empty_list = [1, 2, 2, 3, 4, 3, 5, 6, 6, 1]
-empty_list.remove()   
+# empty_list = [1, 2, 2, 3, 4, 3, 5, 6, 6, 1]
+# empty_list.remove()   
     
