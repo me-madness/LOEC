374415e26907ce20eb9141fbe3b5db4f95ce77d9
me_madness <p_rangel@abv.bg>
2023-08-14 19:14:18 +0200

    install numpy

--- Lesson_14_08_23/main_file.py
+++ Lesson_14_08_23/main_file.py
@@ -1,3 +1,6 @@
+from numpy import array
+import numpy as np
+
 table_one_list = [[6, 4, 3, 2],
                   [3, 4, 5, 6]]
 
@@ -9,6 +12,9 @@ table_three_list = []
 for row1 in range(len(table_one_list)):
     temp_list = []
     for row2 in range(len(table_one_list[0])):
-        table_three_list.append(table_three_list[row1][row2] + table_two_list[row1][row2])
+        table_three_list.append(table_one_list[row1][row2] + table_two_list[row1][row2])
     table_three_list.append(temp_list)
-print(table_three_list)    
+print(table_three_list)
+
+
+table_one_list_numpy = array(table_one_list)    
