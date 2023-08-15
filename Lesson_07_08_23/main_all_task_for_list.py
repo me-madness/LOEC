fe84938e263a9805b844e60eba8550ef85fb4fd8
me_madness <p_rangel@abv.bg>
2023-08-15 13:52:10 +0200

    Finish with all task

--- Lesson_07_08_23/main_all_task_for_list.py
+++ Lesson_07_08_23/main_all_task_for_list.py
@@ -25,7 +25,7 @@
 
 # 4. Fourth task Iterate to the list
 
-# # First option
+# # # First option
 # empty_list = [5, 4, 1.1, 1.5, 1.7, 13, 4, 6]
 # for i in empty_list:
 #     print(f"{i} is the list element")
@@ -58,10 +58,10 @@
 # # 6.1 First Way
 # print("Please, enter a number")
 # a = int(input()) 
-#    if a in empty_list:
-#        print(a, "exists")
-#    else:
-#        print(a, "Does not exist")         
+# if a in empty_list:
+#     print(a, "exists")
+# else:
+#     print(a, "Does not exist")         
 # # 6.2 Second Way    
 # print("Please, enter a number")
 # while True:
@@ -85,17 +85,7 @@
     
 # # 6.4 Forth way         
 # print("Please, enter a number")
-# value_check = []   
-# def check_presence(value_check):               
-#     for value in value_check:
-#         if value == empty_list:
-#             print(f'{value} is presented in the list')
-#         else:
-#             print(f'{value} is  not presented in the list') 
-
-# check_presence(1)                      
-# check_presence(8)                      
-# check_presence(9)   
+# ws  
                    
 # # 7. Seven task count the occurrences 
 
@@ -105,7 +95,7 @@
 # second = empty_list.count(5) 
 # print(f'empty_list contains {first} "value 3" element')  
 # print(f'empty_list contains {second} "value 5" element')  
-# # First option
+# # Second option
 # 5 in empty_list
 # empty_list.count(3,5) != 0
 
@@ -113,11 +103,10 @@
 
 # empty_list = [1, 3, 5, 8, 6, 5, 6, 2, 5]
 # first = empty_list.index(5)
-# second = empty_list.index()
-# if first != True:
-#     print(f'{first} Index is not in the List')
+# if first == False:
+#     print(f'Index is not in the List')
 # else:
-#     print(f'{first} index is exist in the List')    
+#     print(f'Index {first} exist in the List')    
      
 # # 9. Ninth task  reverse the order
 
@@ -131,68 +120,90 @@
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0]
 # new_list = empty_list.sort()
-# print(empty_list)
+# empty_list.reverse()
 # print(sorted(empty_list))
+# print(empty_list)
 
 # # 11. Eleventh task remove elements
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0]
-# first_list = empty_list.remove(1)
-# second_list = empty_list.pop(1)
+# first_list = empty_list.remove(3)
+# second_list = empty_list.pop()
 # print(empty_list)
 
 # # 12. Twelve task  clear all elements
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-# new_list = empty_list.clear()
-# print(new_list)
+# empty_list.clear()
+# print(empty_list)
 
 # # 13. Thirteen task  create a copy of the list 
 
-empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-empty_list_two = []
-empty_list_two.copy(empty_list)
-print(empty_list_two)
+# empty_list = [1, 3, 5, 8, 6, 9, 0]
+# empty_list_two = empty_list.copy()
+# print(empty_list_two)
 
 # # 14. Fourteen task concatenate two lists 
 
 # empty_list_one = [1, 3, 5, 8, 6, 9, 0]
 # empty_list_two = [ 6, 9, 2, 10]
+# print(empty_list_one + empty_list_two)
 
 
 # # 15. Fifteen task use list comprehension
 
+# First way
 # empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-# squares[empty_list]
+# squares_list = []
+# for i in empty_list:
+#     squares_list = i ** 2
+#     print(squares_list)
+# Second way
+# empty_list = [1, 3, 5, 8, 6, 9, 0 ]
+# new_list = [x**2 for x in empty_list]
+# print(new_list)    
 
 # # 16. Sixteen task combine two lists
 
 # empty_list_one = [1, 2, 3]
 # empty_list_two = ['a', 'b', 'c']
-# zip()
+# result = zip(empty_list_one, empty_list_two)
+# empty_list = list(result)
+# print(empty_list)
 
 # # 17. Seventeen task find the maximum and minimum elements  
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0 ]
-# max()
-# min()
+# max_number = max(empty_list)
+# min_number = min(empty_list)
+# print(max_number)
+# print(min_number)
 
 # # 18. Eighteen task calculate the sum
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0]
-# sum()
+# result = sum(empty_list)
+# print(result)
 
 # # 19. Nineteen task use the enumerate()
 
 # empty_list = [1, 3, 5, 8, 6, 9, 0]
-# enumerate()
+# for i in enumerate(empty_list):
+#     print(i)
 
 # # 20. Twenty task use list unpacking 
 
 # empty_list = [1, 3, 5, 8]
+# a, b, c, d = empty_list
+# print(a)
+# print(b)
+# print(c)
+# print(d)
 
 # # 21. Twenty-one task remove duplicate elements 
 
 # empty_list = [1, 2, 2, 3, 4, 3, 5, 6, 6, 1]
+# empty_list = list(set(empty_list))
+# print(empty_list)
 # empty_list.remove()   
     
