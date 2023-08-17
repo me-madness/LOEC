8621dffa546d122ca5807ad8c3b0c11df77cd00e
me_madness <p_rangel@abv.bg>
2023-08-17 12:45:46 +0200

    02. Finish

--- Lesson_08_08_23/Dictionaries/main_all_task_for_dictionaries.py
+++ Lesson_08_08_23/Dictionaries/main_all_task_for_dictionaries.py
@@ -2,68 +2,89 @@
 
 # # 01. First task Create an empty dictionary
 
-empty_dict = {}
+# empty_dict = {}
+# empty_dict['name'] = 'John'
+# empty_dict['age'] = 35
+# empty_dict['city'] = 'New York'
+# empty_dict['email'] = 'john@example.com'
+# print(empty_dict)
 
-# # 2. Second task
+# # 2. Second task Access values
 
-empty_dict = {}
+empty_dict = {'name': 'John', 'age': 35, 'city': 'New York', 'email': 'john@example.com'}
 
-# # 3. Third task
+dict1 = empty_dict['name']
+dict2 = empty_dict['email']
 
-empty_dict = {}
+print(f'name: {dict1}')
+print(f'email: {dict2}')
 
-# # 4. Fourth task
-
-empty_dict = {}
-
-# # 5. Fifth task
-
-empty_dict = {}
-
-# # 6. Sixth task
-
-empty_dict = {}
-
-# # 7. Seven task
-
-empty_dict = {}
-
-# # 8. Eighth task
-
-empty_dict = {}
-
-# # 9. Ninth task
+# new_dict =  {}
+# for i in empty_dict:
+#     if i == 'name':
+#         new_dict[i]
+#     elif i == 'email':
+#         new_dict[i]
+#     else:
+#         print('The key is not in Dictionaries') 
+#         print(str(new_dict))               
+# # 3. Third task Check if a key exists
 
 empty_dict = {}
 
-# # 10. Tenth task sort the list
+# # 4. Fourth task Iterate over the dictionary
 
 empty_dict = {}
 
-# # 11. Eleventh task
+# # 5. Fifth task Get the number of key-value pairs
 
 empty_dict = {}
 
-# # 12. Twelve task
+# # 6. Sixth task Get a list of all keys
 
 empty_dict = {}
 
-# # 13. Thirteen task
+# # 7. Seven task Get a list of all values
 
 empty_dict = {}
 
-# # 14. Fourteen task
+# # 8. Eighth task  Get a list of key-value pairs
 
 empty_dict = {}
 
-# # 15. Fifteen task
+# # 9. Ninth task Update or add new key-value pairs
 
 empty_dict = {}
 
-# # 16. Sixteen task
+# # 10. Tenth task Access values
 
 empty_dict = {}
 
-# # 17. Seventeen task
+# # 11. Eleventh task Dictionary comprehension 
+
+empty_dict = {}
+
+# # 12. Twelve task Use the pop() 
+
+empty_dict = {}
+
+# # 13. Thirteen task Use the popitem()
+
+empty_dict = {}
+
+# # 14. Fourteen task Use the clear()
+
+empty_dict = {}
+
+# # 15. Fifteen task Use the copy()
+
+empty_dict = {}
+
+# # 16. Sixteen task Use the update()
+
+empty_dict = {}
+
+# # 17. Seventeen task Use the dict()
+
 empty_dict = {}
 
